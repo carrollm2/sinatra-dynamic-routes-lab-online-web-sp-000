c588f67530d927fbb690d1f710c3599591458f2b
Michael Carroll <carrollm2@gmail.com>
2020-02-23 21:45:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,17 +11,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    # count = params[:number].to_i
-    # count.times do
-    #   phrase * number.to_i
-    # end
-    # result = ""
-    # number.to_i.times do |i|
-    #   result += phrase.upcase
-    # end
-    # result
     params["phrase"] * params[:number].to_i
-
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
