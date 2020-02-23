722d7e6cdc36939f04b92cdb778d450f49ea6f4a
Michael Carroll <carrollm2@gmail.com>
2020-02-23 21:30:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     count = params[:number].to_i
-    count.times {"peace"}
+    "#{count}"
+    # count.times {"peace"}
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
